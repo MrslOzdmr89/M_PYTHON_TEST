bb3d9626ffda861292434650fb94aba210e6e0ca
Mürsel Özdemir <mursel.ozdemir@example.com>
2026-03-24 10:22:02 +0300

    menu.app

---
+++ menu_app.py
@@ -0,0 +1,13 @@
+print("Destek Bilgi Sistemi")
+
+print("1- KOSGEB")
+print("2- TÜBİTAK")
+
+secim = input("Seçim yap: ")
+
+if secim == "1":
+    print("KOSGEB destekleri görüntüleniyor")
+elif secim == "2":
+    print("TÜBİTAK destekleri görüntüleniyor")
+else:
+    print("Geçersiz seçim")
