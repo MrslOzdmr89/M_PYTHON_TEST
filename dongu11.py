ce48b9cbf6e5528eb26606701d2d233768f58eab
Mürsel Özdemir <mursel.ozdemir@example.com>
2026-03-27 17:07:54 +0300

    piramit

---
+++ dongu11.py
@@ -0,0 +1,42 @@
+"""for i in range(1, 11):
+    for j in range(1, 11):
+        print(i, "*", j, "=", i * j)"""
+
+
+"""print("*", end="")
+print()
+print("*", "*", end="")
+print()
+print("*", "*", "*", end="")
+print()
+print("*", "*", "*", "*", end="")
+print()
+print("*", "*", "*", "*", "*", end="")"""
+
+
+"""print("*", end="")
+print()
+print("**", end="")
+print()
+print("***", end="")
+print()
+print("****", end="")
+print()
+print("*****", end="")"""
+
+"""print("*****", end="")
+print()
+print("****", end="")
+print()
+print("***", end="")
+print()
+print("**", end="")
+print()
+print("*", end="")"""
+
+"""for i in range(5, 0, -1):
+    print("*" * i)"""
+
+for i in range (5, 0, -1):
+    for j in range(1, 0, -1):
+        print("*"*i)
